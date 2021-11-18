c693f573db896917366d2b1ac65d3bd76ba56f90
Andrew Graham <andrewsgraham1995@gmail.com>
2021-11-18 11:54:06 -0800

    Let's hard-code in the registry auth.

--- infrastructure/harbor/pre-reqs.sh
+++ infrastructure/harbor/pre-reqs.sh
@@ -1,10 +1,22 @@
 #!/bin/zsh
 
 kubectl create ns harbor
+kubectl apply -f infrastructure/harbor/my_service.yaml
+kubectl create secret docker-registry regcred \
+    --docker-server=https://harbor.127.0.0.1.nip.io:8443 \
+    --docker-username=admin \
+    --docker-password=Harbor12345 \
+    -n jenkins
+
 helm repo add harbor https://helm.goharbor.io  
 
 echo "Installing Harbor..."
-helm upgrade my-harbor harbor/harbor -i --wait -n harbor -f infrastructure/harbor/my_values.yaml --timeout 10m0s
+helm upgrade my-harbor harbor/harbor -i -n harbor -f infrastructure/harbor/my_values.yaml
+echo "Giving Harbor 3 minutes to stand up"
+sleep 180
+
+# Do I auto-add the nip.io rewrite for coredns?
+#if grep -q "nip.io"  
 
 # Create projects in Harbor via API
 baseURL='harbor.127.0.0.1.nip.io:8443/api/v2.0/'

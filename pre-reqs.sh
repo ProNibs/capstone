d69ad3e06272ba47144d078d12e1f6170c5cb0ff
Andrew Graham <andrewsgraham1995@gmail.com>
2021-11-18 07:06:52 -0800

    Finalized pre-reqs script.

--- pre-reqs.sh
+++ pre-reqs.sh
@@ -12,3 +12,19 @@ source ./infrastructure/kapp-controller-sa/pre-reqs.sh
 # Install Jenkins
 kapp deploy -y -a jenkins.kapp -f infrastructure/jenkins/app/
 
+# Create config-test file from config file
+rm ~/.kube/config-test
+sed -E 's/127.0.0.1:[0-9]+/kubernetes.default/g' ~/.kube/config > ~/.kube/config-test
+
+# Run nip.io for local sanity and hostname resolving without editing /etc/hosts like a madman
+# Only needs to be done last night
+#cd nip.io && source ./build_and_run_docker.sh
+
+# Reminder for next steps
+echo "Remember to create a jenkins secret file with ID=kube-config-v2 based on ~/.kube/config-test"
+
+# Get Jenkins admin password
+echo "Jenkins Password:" 
+kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo
+
+

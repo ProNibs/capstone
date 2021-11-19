bf540ba4c37d9fad4a68c5ea81f7c5a3e64ed989
Andrew Graham <andrewsgraham1995@gmail.com>
2021-11-19 10:52:43 -0800

    update metallb pre-reqs script to ensure namespace is present.

--- infrastructure/metallb/pre-reqs.sh
+++ infrastructure/metallb/pre-reqs.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+# Need this made before can put a secret there
+kubectl create namespace metallb-system
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" 
 
 kubectl get configmap kube-proxy -n kube-system -o yaml | \
